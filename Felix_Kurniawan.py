a0d07c46be1bd4abbc552af90bfe654c940ce68e
felixkur1 <fk494154@gmail.com>
2024-09-04 22:03:58 +0700

    ga ganti apa apa

---
+++ Felix_Kurniawan.py
@@ -0,0 +1,10 @@
+print("Hello Wolrd!")
+a,b,c = 10,15,5
+pi = 22/7
+def hitung_luas_lingkaran(sisi):
+    luas = sisi * sisi * pi
+    print(luas)
+
+hitung_luas_lingkaran(a)
+hitung_luas_lingkaran(b)
+hitung_luas_lingkaran(c)
