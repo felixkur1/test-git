874f59b0b3d03c8c6765df94a4dd1b6a6496f8f4
felixkur1 <fk494154@gmail.com>
2024-09-05 20:46:49 +0700

    menambah fungsi hitung keliling lingkaran

--- Felix_Kurniawan.py
+++ Felix_Kurniawan.py
@@ -1,10 +1,18 @@
 print("Hello Wolrd!")
 a,b,c = 10,15,5
 pi = 22/7
-def hitung_luas_lingkaran(sisi):
-    luas = sisi * sisi * pi
+
+def hitung_luas_lingkaran(radius):
+    luas = radius * radius * pi
     print(luas)
 
+def hitung_keliling_lingkaran(radius):
+    keliling = pi * 2 * radius
+    print(keliling)
+
 hitung_luas_lingkaran(a)
+hitung_keliling_lingkaran(a)
 hitung_luas_lingkaran(b)
+hitung_keliling_lingkaran(b)
 hitung_luas_lingkaran(c)
+hitung_keliling_lingkaran(c)
